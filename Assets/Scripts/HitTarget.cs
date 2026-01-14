b2ecacf57b04ee9012213d747d784391fd10d58c
pgmtx <programutox@disroot.org>
2026-01-15 00:30:36 +0100

    Ajouté un son et commenté le script

--- Assets/Scripts/HitTarget.cs
+++ Assets/Scripts/HitTarget.cs
@@ -1,18 +1,27 @@
+/*
+ * HitTarget.cs
+ * Gère la couleur, les sons et les collisions entre d'une cible par rapport au marteau.
+ */
+
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class HitTarget : MonoBehaviour
 {
     // Les comparaisons par Tag calcule l'id de la chaîne passée en paramètre.
     // Remplacer ce paramètre par son id nous épargne ces calculs.
     private static readonly int Color1 = Shader.PropertyToID("_Color");
-    public float hitForceThreshold = 1.5f;
+    // public float hitForceThreshold = 1.5f;
 
     public Renderer targetRenderer;
     public Color hitColor = Color.red;
     private Color _normalColor;
 
+    private AudioSource _audioSource;
+    
     private void Start()
     {
+        _audioSource = GetComponent<AudioSource>();
         _normalColor = targetRenderer.material.GetColor(Color1);
     }
 
@@ -21,6 +30,7 @@ public class HitTarget : MonoBehaviour
         if (other.CompareTag("Hammer"))
         {
             targetRenderer.material.SetColor(Color1, hitColor);
+            _audioSource.Play();
         }
     }
 
