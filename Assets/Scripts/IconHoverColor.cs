2d95cb33d82cafb446aeb7dea765442b127c8340
pgmtx <programutox@disroot.org>
2026-01-15 23:01:53 +0100

    Modifié le retour de sélection de sort avec un contour plutôt qu'une teinture

--- Assets/Scripts/IconHoverColor.cs
+++ Assets/Scripts/IconHoverColor.cs
@@ -7,25 +7,21 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
 
-[RequireComponent(typeof(Image), typeof(XRBaseInteractable))]
+[RequireComponent(typeof(Outline), typeof(XRBaseInteractable))]
 public class IconHoverColor : MonoBehaviour
 {
-    [SerializeField] private Color normalColor = Color.white;
-    [SerializeField] private Color hoverColor = Color.cyan;
-
-    private Image _image;
+    private Outline _outline;
 
     private void Awake()
     {
-        _image = GetComponent<Image>();
-        _image.color = normalColor;
+        _outline = GetComponent<Outline>();
+        _outline.enabled = false;
 
         var interactable = GetComponent<XRBaseInteractable>();
-        interactable.hoverEntered.AddListener(_ => _image.color = hoverColor);
-        interactable.hoverExited.AddListener(_ => _image.color = normalColor);
+        interactable.hoverEntered.AddListener(_ => _outline.enabled = true);
+        interactable.hoverExited.AddListener(_ => _outline.enabled = false);
         
         // activated = quand on appuie sur la gâchette
         interactable.activated.AddListener(_ => Debug.Log("press"));
-        interactable.selectEntered.AddListener(_ => Debug.Log("grab"));
     }
 }
